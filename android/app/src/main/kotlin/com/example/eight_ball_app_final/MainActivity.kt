437b8f41e64cab552e917359106df8590df96731
sanzhay <asiabalatech@gmail.com>
2021-05-19 01:31:55 +0600

    add new app

---
+++ android/app/src/main/kotlin/com/example/eight_ball_app_final/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eight_ball_app_final
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask me anything'),
+        backgroundColor: Colors.blue.shade800,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballImage = 1;
+
+  void changeBall() {
+    setState(() {
+      ballImage = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FlatButton(
+            onPressed: () {
+              changeBall();
+            },
+            child: Image.asset('assets//img/b$ballImage.png')),
+      ),
+    );
+  }
+}
